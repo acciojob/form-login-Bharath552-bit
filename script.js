f3888e6b054423958db6ded620dce0b517fa891b
Bharath552-bit <patthembharath32@gmail.com>
2026-01-05 17:05:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault();
+	const first=event.target.fname.value;
+	const last=event.target.lname.value;
+	alert(`${first} ${last}`)
 
 }
